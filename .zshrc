43527084f78f24a20371655c1d65d76501a9a42e
riz1 <rizwan.k.off@gmail.com>
2025-12-07 21:50:36 +0530

    added gh cli to clitools.sh lazygit install files cleanup

--- .zshrc
+++ .zshrc
@@ -46,3 +46,4 @@ setopt hist_verify
 
 #bun
 export PATH="$HOME/.bun/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
--- install-scripts/clitools.sh
+++ install-scripts/clitools.sh
@@ -68,7 +68,24 @@ else
     print_success "Lazygit installed successfully"
 fi
 
-# 4: Install Tmux and Tmux Plugin Manager
+# 4: Install GitHub CLI
+print_progress "Installing GitHub CLI..."
+if command -v gh >/dev/null 2>&1; then
+    print_warning "GitHub CLI is already installed"
+else
+   (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+    && sudo mkdir -p -m 755 /etc/apt/keyrings \
+    && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+    && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+    && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+    && sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+    print_success "GitHub CLI installed successfully"
+fi
+
+# 5: Install Tmux and Tmux Plugin Manager
 print_progress "Installing Tmux and Tmux Plugin Manager..."
 if command -v tmux >/dev/null 2>&1; then
     print_warning "Tmux is already installed"
